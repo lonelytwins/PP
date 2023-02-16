462a642cd3f65c57d17afdcaaabdde69f2f55a8f
Denis Kopishev <deniskopishev@gmail.com>
2023-02-16 14:13:25 +0300

    Added comma.

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Git
+{
+    class Git
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter your first name :");
+            string first_name = Console.ReaLine();
+            System.Console.WriteLine("Now enter your last name : ");
+            string last_name = Console.Readline();
+            System.Console.WriteLine($"Hello {first_name} {last_name}!");
+        }
+    }
+}
